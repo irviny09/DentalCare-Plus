001f8c59fedf485c4261eefa47569a674cdb7738
Irvin Marquez <irvinmarquez@192.168.1.23>
2026-03-07 21:59:15 -0600

    Setup de aplicacion

---
+++ src/main/java/com/ubam/dentcare_plus/DentcarePlusApplication.java
@@ -0,0 +1,13 @@
+package com.ubam.dentcare_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentcarePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentcarePlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubam/dentcare_plus/DentcarePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubam.dentcare_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentcarePlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
